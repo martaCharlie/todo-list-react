4a6d1371e66ddda405f788c2303a4637c1d3c1bd
Marta Wojcik <marta.bus@onet.pl>
2023-03-03 09:47:59 +0100

    hideDoneTasks changed to hideDone

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const tasks = [
   { id: 2, content: "odrobaczyć psa", done: true },
 ];
 
-const hideDoneTasks = false;
+const hideDone = false;
 
 function App() {
   return (
@@ -19,10 +19,8 @@ function App() {
       <Section title="Dodaj zadanie" body={<Form />} />
       <Section
         title="Do zrobienia"
-        body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
-        extraHeaderContent={
-          <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
-        }
+        body={<Tasks tasks={tasks} hideDone={hideDone} />}
+        extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} />}
       />
     </Container>
   );
