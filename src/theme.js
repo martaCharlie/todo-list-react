ed354b989b4cf1f6fcfb7bfbe1ba00f973fbf9db
Marta Wojcik <marta.bus@onet.pl>
2023-05-24 10:23:34 +0200

    theme.js created

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+  color: {
+    black: "#000",
+    white: "#fff",
+    gray: "#ccc",
+    grayish: "#ddd",
+    gallery: "#eee",
+    teal: "#008080",
+    crimson: "#db143c",
+    forestGreen: "#228c22",
+  },
+  breakpoint: {
+    mobileFeature: 767,
+  },
+};
