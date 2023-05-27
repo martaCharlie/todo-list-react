25bc7ec6186feadbdbddc177aa855ca9aa50a5d7
Marta Wojcik <marta.bus@onet.pl>
2023-05-27 14:02:31 +0200

    @ media updated

--- src/Buttons/styled.js
+++ src/Buttons/styled.js
@@ -1,21 +1,24 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 export const Button = styled.div`
   display: flex;
   flex-wrap: wrap;
+  flex-direction: row;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileFeature}px) {
+    justify-content: center;
+    flex-direction: column;
+  }
 `;
 
 export const StyledButton = styled.button`
+  cursor: pointer;
   background: transparent;
   color: ${({ theme }) => theme.color.teal};
   border: none;
   margin: 0 0 0 20px;
   transition: filter 0.3s;
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobileFeature}) {
-    flex-basis: 100%;
-    margin: 10px;
-  }
+  flex-wrap: wrap;
 
   &:hover {
     filter: brightness(120%);
@@ -23,5 +26,10 @@ export const StyledButton = styled.button`
 
   &:disabled {
     color: ${({ theme }) => theme.color.gray};
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileFeature}) {
+    align-self: center;
+    flex-basis: 100%;
   }
 `;
